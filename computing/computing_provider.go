d97b0f9aed9eaba9938c9a06f50ea5c92a5fda53
Charles Cao <nebulablock@gmail.com>
2023-04-23 04:11:26 -0400

    update the hearbeat

--- computing/computing_provider.go
+++ computing/computing_provider.go
@@ -9,18 +9,60 @@ import (
 	"github.com/filswan/go-swan-lib/logs"
 	"go-computing-provider/models"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
-func InitComputingProvider() {
+func updateProviderInfo(nodeID string, peerID string, address string) {
+	// Replace the following URL with your Flask application's /cp endpoint URL
+	updateURL := "http://localhost:5002/cp"
+
+	data := url.Values{
+		"name":          {"Provider Local"},
+		"node_id":       {nodeID},
+		"multi_address": {"/ip4/127.0.0.1/tcp/8085"},
+		"autobid":       {"1"},
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", updateURL, strings.NewReader(data.Encode()))
+	if err != nil {
+		logs.GetLogger().Errorf("Error creating request: %v", err)
+		return
+	}
+
+	// Set the content type and API token in the request header
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("LAGRANGE_API_TOKEN"))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logs.GetLogger().Errorf("Error updating provider info: %v", err)
+	} else {
+		logs.GetLogger().Infof("Provider info sent. Status code: %d\n", resp.StatusCode)
+		if resp.StatusCode == 400 {
+			logs.GetLogger().Info(resp.Body)
+		}
+
+		err := resp.Body.Close()
+		if err != nil {
+			logs.GetLogger().Errorf(err.Error())
+			return
+		}
+	}
+}
+func InitComputingProvider() string {
 	nodeID, peerID, address := generateNodeID()
 
 	logs.GetLogger().Infof("Node ID :%s Peer ID:%s address:%s",
 		nodeID,
-		peerID, address,
-	)
+		peerID, address)
+	updateProviderInfo(nodeID, peerID, address)
+	return nodeID
 }
 func generateNodeID() (string, string, string) {
 	privateKeyPath := ".swan_node/private_key"
--- initializer/initializer.go
+++ initializer/initializer.go
@@ -1,10 +1,15 @@
 package initializer
 
 import (
+	"fmt"
 	"github.com/filswan/go-swan-lib/logs"
 	"github.com/joho/godotenv"
 	"go-computing-provider/computing"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func LoadEnv() {
@@ -16,7 +21,46 @@ func LoadEnv() {
 	logs.GetLogger().Info("name: ", os.Getenv("MCS_BUCKET"))
 }
 
+func sendHeartbeat(nodeId string) {
+	// Replace the following URL with your Flask application's heartbeat endpoint URL
+	heartbeatURL := "http://localhost:5002/cp/heartbeat"
+	payload := strings.NewReader(fmt.Sprintf(`{
+    "node_id": "%s",
+    "status": "Active"
+}`, nodeId))
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", heartbeatURL, payload)
+	if err != nil {
+		logs.GetLogger().Errorf("Error creating request: %v", err)
+		return
+	}
+	// Set the API token in the request header (replace "your_api_token" with the actual token)
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("LAGRANGE_API_TOKEN"))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		logs.GetLogger().Errorf("Error sending heartbeat: %v", err)
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		logs.GetLogger().Infof("Heartbeat sent. Status code: %d\n %s", resp.StatusCode, string(body))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func sendHeartbeats(nodeId string) {
+	ticker := time.NewTicker(5 * time.Second)
+	for range ticker.C {
+		sendHeartbeat(nodeId)
+	}
+}
 func ProjectInit() {
 	LoadEnv()
-	computing.InitComputingProvider()
+	nodeID := computing.InitComputingProvider()
+	// Start sending heartbeats
+	go sendHeartbeats(nodeID)
+
 }

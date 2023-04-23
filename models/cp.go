d4a4d77d3c49c5eca532cac0ec6a43cb0de2d849
Charles Cao <nebulablock@gmail.com>
2023-04-23 11:54:38 -0400

    Accept job api

--- models/cp.go
+++ models/cp.go
@@ -7,3 +7,17 @@ type ComputingProvider struct {
 	Autobid       int    `json:"autobid"`
 	WalletAddress int    `json:"wallet_address"`
 }
+
+type JobData struct {
+	UUID          string `json:"uuid"`
+	Name          string `json:"name"`
+	Status        string `json:"status"`
+	Duration      int    `json:"duration"`
+	Hardware      string `json:"hardware"`
+	JobSourceURI  string `json:"job_source_uri"`
+	JobResultURI  string `json:"job_result_uri"`
+	StorageSource string `json:"storage_source"`
+	TaskUUID      string `json:"task_uuid"`
+	CreatedAt     string `json:"created_at"`
+	UpdatedAt     string `json:"updated_at"`
+}
--- routers/cp_router.go
+++ routers/cp_router.go
@@ -1,9 +1,12 @@
 package routers
 
 import (
+	"github.com/filswan/go-swan-lib/logs"
 	"github.com/gin-gonic/gin"
 	"go-computing-provider/common"
 	"go-computing-provider/computing"
+	"go-computing-provider/models"
+	"log"
 	"net/http"
 )
 
@@ -12,7 +15,28 @@ func GetServiceProviderInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, common.CreateSuccessResponse(info))
 }
 
+func receiveJob(c *gin.Context) {
+	var jobData models.JobData
+
+	if err := c.ShouldBindJSON(&jobData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	log.Printf("Job received: %+v\n", jobData)
+
+	// TODO: Async Processing the job
+	result := processJob(jobData)
+
+	c.JSON(http.StatusOK, result)
+}
+func processJob(jobData models.JobData) interface{} {
+	// Add your job processing logic here
+	logs.GetLogger().Info("Processing job: %+v\n", jobData)
+	return nil
+}
 func CPManager(router *gin.RouterGroup) {
 
 	router.GET("/host/info", GetServiceProviderInfo)
+	router.POST("/lagrange/jobs", receiveJob)
 }

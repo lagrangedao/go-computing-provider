4cd1c9a9b561549b71130cc3f66e03564b857aeb
sonic <longzhang>
2023-07-19 18:39:44 +0800

    add storage resource

--- computing/resources_sumary.go
+++ computing/resources_sumary.go
@@ -17,6 +17,7 @@ const (
 
 	Nvidia_Gpu_Num string = "nvidia.com/gpu"
 	Cpu_Model      string = "feature.node.kubernetes.io/cpu-model.vendor_id"
+	Arch_Model     string = "beta.kubernetes.io/arch"
 )
 
 func allActivePods(clientSet *kubernetes.Clientset) ([]corev1.Pod, error) {
@@ -31,30 +32,39 @@ func allActivePods(clientSet *kubernetes.Clientset) ([]corev1.Pod, error) {
 
 func getNodeResource(allPods []corev1.Pod, node *corev1.Node) (*models.NodeResource, error) {
 	var (
-		allocatedCPU int64
-		allocatedMem int64
+		usedCpu     int64
+		usedMem     int64
+		usedStorage int64
 	)
 
 	var nodeResource = new(models.NodeResource)
 	nodeResource.MachineId = node.Status.NodeInfo.MachineID
+	nodeResource.Model = node.Labels[Arch_Model]
 
 	for _, pod := range getPodsFromNode(allPods, node) {
-		allocatedCPU += cpuInPod(&pod)
-		allocatedMem += memInPod(&pod)
+		usedCpu += cpuInPod(&pod)
+		usedMem += memInPod(&pod)
+		usedStorage += storageInPod(&pod)
 	}
 
 	nodeResource.Cpu.Total = strconv.FormatInt(node.Status.Capacity.Cpu().Value(), 10)
-	nodeResource.Cpu.Used = strconv.FormatInt(allocatedCPU, 10)
-	nodeResource.Cpu.Free = strconv.FormatInt(node.Status.Capacity.Cpu().Value()-allocatedCPU, 10)
+	nodeResource.Cpu.Used = strconv.FormatInt(usedCpu, 10)
+	nodeResource.Cpu.Free = strconv.FormatInt(node.Status.Capacity.Cpu().Value()-usedCpu, 10)
 
 	nodeResource.Vcpu.Total = nodeResource.Cpu.Total
 	nodeResource.Vcpu.Used = nodeResource.Cpu.Used
 	nodeResource.Vcpu.Free = nodeResource.Cpu.Free
 
-	nodeResource.Memory.Total = fmt.Sprintf("%.2f GiB", float64(node.Status.Capacity.Memory().Value()/1024/1024/1024))
-	nodeResource.Memory.Used = fmt.Sprintf("%.2f GiB", float64(allocatedMem/1024/1024/1024))
-	freeMemory := node.Status.Capacity.Memory().Value() - allocatedMem
+	nodeResource.Memory.Total = fmt.Sprintf("%.2f GiB", float64(node.Status.Allocatable.Memory().Value()/1024/1024/1024))
+	nodeResource.Memory.Used = fmt.Sprintf("%.2f GiB", float64(usedMem/1024/1024/1024))
+	freeMemory := node.Status.Capacity.Memory().Value() - usedMem
 	nodeResource.Memory.Free = fmt.Sprintf("%.2f GiB", float64(freeMemory/1024/1024/1024))
+
+	nodeResource.Storage.Total = fmt.Sprintf("%.2f GiB", float64(node.Status.Allocatable.Storage().Value()/1024/1024/1024))
+	nodeResource.Storage.Used = fmt.Sprintf("%.2f GiB", float64(usedStorage/1024/1024/1024))
+	freeStorage := node.Status.Allocatable.Storage().Value() - usedStorage
+	nodeResource.Storage.Free = fmt.Sprintf("%.2f GiB", float64(freeStorage/1024/1024/1024))
+
 	return nodeResource, nil
 }
 
@@ -67,16 +77,16 @@ func getPodsFromNode(allPods []corev1.Pod, node *corev1.Node) (pods []corev1.Pod
 	return pods
 }
 
-func gpuInPod(pod *corev1.Pod) (gpuCount int64) {
+func storageInPod(pod *corev1.Pod) (storageUsed int64) {
 	containers := pod.Spec.Containers
 	for _, container := range containers {
-		val, ok := container.Resources.Limits[corev1.ResourceName(Nvidia_Gpu_Num)]
+		val, ok := container.Resources.Requests[corev1.ResourceStorage]
 		if !ok {
 			continue
 		}
-		gpuCount += val.Value()
+		storageUsed += val.Value()
 	}
-	return gpuCount
+	return storageUsed
 }
 
 func cpuInPod(pod *corev1.Pod) (cpuCount int64) {
